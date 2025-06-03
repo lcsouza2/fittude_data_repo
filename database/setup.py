e66c550d25a0ad3c9ad4a0ee79b52f8b2be4e309
Luiz Carlos Souza Garcia <lcsouzagarcia@gmail.com>
2025-06-03 01:21:04 -0300

    chore: removed unused import

--- database/setup.py
+++ database/setup.py
@@ -1,6 +1,5 @@
 from mapping import reg
 from sqlalchemy import create_engine
-from sqlite3 import connect
 
 engine = create_engine("sqlite:///fittude_repo.db")
 reg.metadata.create_all(bind=engine)
