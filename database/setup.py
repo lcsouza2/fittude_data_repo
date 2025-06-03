53313bebe26a6eb31fe9424a3a92e241cbb70ba2
Luiz Carlos Souza Garcia <lcsouzagarcia@gmail.com>
2025-06-03 02:18:30 -0300

    feat: database/setup.py now also sets default data

--- database/setup.py
+++ database/setup.py
@@ -1,6 +1,10 @@
 from mapping import reg
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
+import sqlite3
 
-engine = create_engine("sqlite:///fittude_repo.db")
+engine = create_engine("sqlite:///.fittude_repo.db")
 reg.metadata.create_all(bind=engine)
 
+with open("./database/default_data.sql", "r") as sql_file:
+    with sqlite3.connect(".fittude_repo.db") as conn:
+        conn.executescript(sql_file.read())
